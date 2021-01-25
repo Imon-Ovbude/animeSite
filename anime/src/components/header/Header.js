362afe9826d51d0b2e1ffe534134313c89f7f5c3
Imon-Ovbude <imonhimio@gmail.com>
2021-01-24 16:24:12 -0800

    added styles to header component

--- anime/src/components/header/Header.js
+++ anime/src/components/header/Header.js
@@ -1,47 +1,61 @@
 import React from 'react';
+import { BiSearchAlt } from 'react-icons/bi';
+import { BsBookmark } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { RiArrowDownSFill } from 'react-icons/ri';
 import './header.css';
 
 function Header() {
   return (
     <header className='header'>
-      <ul>
-        {/* <li>
-          <a href='#'>
-            <div style={{ backgroundColor: 'black', width: '20px' }}></div>
-            <div></div>
-            <div></div>
-          </a>
-        </li> */}
-        <li>
-          <a href='#'>AnimeSite</a>
-        </li>
-        <li>
-          <a href='#'>Browse</a>
-        </li>
-        <li>
-          <a href='#'>Seasons</a>
-        </li>
-        <li>
-          <a href='#'>Schedule</a>
-        </li>
-        <li>
-          <a href='#'>Headlines</a>
-        </li>
-        <li>
-          <a href='#'>Videos</a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a href='#'>search icon</a>
-        </li>
-        <li>
-          <a href='#'>library icon</a>
-        </li>
-        <li>
-          <a href='#'>Log In</a>
-        </li>
-      </ul>
+      <div className='header-container'>
+        <ul className='header-ul'>
+          <li>
+            <a href='#'>
+              <GiHamburgerMenu />
+            </a>
+          </li>
+          <li>
+            <a href='#' className='anime-site'>
+              AnimeSite
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              Browse <RiArrowDownSFill />
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              Seasons <RiArrowDownSFill />
+            </a>
+          </li>
+          <li>
+            <a href='#'>Schedule</a>
+          </li>
+          <li>
+            <a href='#'>Headlines</a>
+          </li>
+          <li>
+            <a href='#'>Videos</a>
+          </li>
+        </ul>
+        <ul className='header-ul'>
+          <li>
+            <a href='#'>
+              <BiSearchAlt />
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <BsBookmark />
+            </a>
+          </li>
+          <li>
+            <a href='#'>Log In</a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
